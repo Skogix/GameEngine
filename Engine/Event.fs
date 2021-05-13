b651e217fad61396a2aeff03e5c77ef5ecba58a9
skogix <Skogix@gmail.com>
2021-05-13 22:19:10 +0200

    0.05eventStore

--- Engine/Event.fs
+++ Engine/Event.fs
@@ -1,9 +1,21 @@
 module Engine.Event
-type EngineEvent<'T>() =
+type iEvent = interface end
+type EventStore() =
+  let mutable events: (string * iEvent) list = []
+  member this.Add event = events <- events @ [event.GetType().Name, event]
+  member this.History = events
+let eventStore = EventStore()
+type EngineEvent<'T when 'T :> iEvent>() =
   static let event = Event<'T>()
   static member Listen<'T>(handler) = event.Publish.Add handler
-  static member Post = event.Trigger
-type testEvent = string
-let testEventListener x = printfn "%s" x
-EngineEvent<testEvent>.Listen testEventListener
-EngineEvent<testEvent>.Post "test"
+  static member Post e =
+    eventStore.Add e
+    event.Trigger e
+    ()
+type TestEvent = Sträng of string interface iEvent
+type TestEvent2 = Test of int interface iEvent
+EngineEvent<TestEvent>.Listen (fun x -> printfn "TestEvent1: %A" x)
+EngineEvent<TestEvent2>.Listen (fun x -> printfn "TestEvent2: %A" x)
+EngineEvent<TestEvent>.Post(Sträng "test")
+EngineEvent<TestEvent2>.Post(Test 5)
+eventStore.History |> printfn "History: %A"
