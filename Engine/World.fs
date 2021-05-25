73bbed31c58e380099dddd2bab263b603f6521b3
skogix <Skogix@gmail.com>
2021-05-25 13:28:19 +0200

    mailboxes

--- Engine/World.fs
+++ Engine/World.fs
@@ -5,6 +5,7 @@ open Engine.Event
 
 type World() =
   let entityManager = EntityManager()
+  let member 
   member this.Command<'c when 'c :> iCommand> command = eEvent.Post command
   member this.Post<'e when 'e :> iEvent> event = eEvent.Post event
   member this.Listen<'e when 'e :> iEvent> event = eEvent.Listen event
