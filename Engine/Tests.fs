712263d072aa2fa2e15e814211ec54a6b701545f
skogix <Skogix@gmail.com>
2021-05-29 23:34:05 +0200

    CreateWorld returnar object World fail

--- Engine/Tests.fs
+++ Engine/Tests.fs
@@ -6,6 +6,7 @@ let tests =
     ptest "CreateWorld returnar object World." {
       Expect.equal (2+2) 4 "2+2"
     }
+    
 //    ptest "World.CreateEntity() returnar entity med Id=0." {
 //      let entity = World.CreateEntity()
 //      Expect.equal 1 entity.Id "EntityId ska vara 0"
