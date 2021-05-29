05f6511ffc4f7f2764934152bdb081e032ab3b20
skogix <Skogix@gmail.com>
2021-05-29 23:33:13 +0200

    0.1 tests

---
+++ Engine/Tests.fs
@@ -0,0 +1,15 @@
+module Engine.Tests
+open Expecto
+
+let tests =
+  testList "EngineTests" [
+    ptest "CreateWorld returnar object World." {
+      Expect.equal (2+2) 4 "2+2"
+    }
+//    ptest "World.CreateEntity() returnar entity med Id=0." {
+//      let entity = World.CreateEntity()
+//      Expect.equal 1 entity.Id "EntityId ska vara 0"
+//    }
+  ]
+
+
