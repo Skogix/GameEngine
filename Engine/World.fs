25a3a89758344ee023af7a5adb0e3dfde6301958
skogix <Skogix@gmail.com>
2021-05-29 23:44:05 +0200

    hantera generationIds fail

--- Engine/World.fs
+++ Engine/World.fs
@@ -1,6 +1,11 @@
 module Engine.World
+type EntityId = int
+type GenerationId = int
 type Entity = {
-  Id: int
+  Id: EntityId
+  Generation: GenerationId
 }
 type World() =
-  member this.CreateEntity() = {Id = 0}
+  member this.CreateEntity() = { Id = 0
+                                 Generation = 0 }
+  member this.DestroyEntity entity = ()
