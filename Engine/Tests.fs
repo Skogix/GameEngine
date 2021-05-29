228c5abf8eac42abd251de1263cba49461edeb7c
skogix <Skogix@gmail.com>
2021-05-29 23:37:17 +0200

    EntityId fail

--- Engine/Tests.fs
+++ Engine/Tests.fs
@@ -2,15 +2,10 @@ module Engine.Tests
 open Expecto
 
 let tests =
-  testList "EngineTests" [
-    ptest "CreateWorld returnar object World." {
-      Expect.equal (2+2) 4 "2+2"
+    test "World.CreateEntity() returnar entity med Id=0." {
+      let entity = World.CreateEntity()
+      Expect.equal 1 entity.Id "EntityId ska vara 0"
     }
-    
-//    ptest "World.CreateEntity() returnar entity med Id=0." {
-//      let entity = World.CreateEntity()
-//      Expect.equal 1 entity.Id "EntityId ska vara 0"
-//    }
   ]
 
 
