0752dbb6e697f289dbeaded13e589221f1c39ef0
skogix <Skogix@gmail.com>
2021-05-30 15:24:54 +0200

    filters returnar rätt count done

--- Engine/Filter.fs
+++ Engine/Filter.fs
@@ -1,5 +1,26 @@
 module Engine.Filter
 
-let filter1<'a>() = [1]
-let filter2<'a, 'b>() = [1;2]
-let filter3<'a, 'b, 'c>() = [1;2;3]
+open System
+open Engine.Component
+open Engine.Domain
+
+let filter1<'a>() =
+  let a: Component<'a> list = ComponentPool<'a>.GetAllComponents()
+  let e: Entity list = ComponentPool<'a>.GetAllEntities()
+  (a, e)
+let filter2<'a, 'b>() =
+  let es =
+    ComponentPool<'a>.GetAllEntities()
+    |> ComponentPool<'b>.FilterEntities
+  let a = ComponentPool<'a>.GetAllComponentsFromEntities(es)
+  let b = ComponentPool<'b>.GetAllComponentsFromEntities(es)
+  (es, a, b)
+let filter3<'a, 'b, 'c>() =
+  let es =
+    ComponentPool<'a>.GetAllEntities()
+    |> ComponentPool<'b>.FilterEntities
+    |> ComponentPool<'c>.FilterEntities
+  let a = ComponentPool<'a>.GetAllComponentsFromEntities(es)
+  let b = ComponentPool<'b>.GetAllComponentsFromEntities(es)
+  let c = ComponentPool<'c>.GetAllComponentsFromEntities(es)
+  (es, a, b, c)
